cb0891f926a83a4c988d6e76dbaebb4b131c4dd8
sip615 <sunilpatel.328@gmail.com>
2020-03-20 17:52:13 -0400

    updated Train class

--- train.js
+++ train.js
@@ -9,4 +9,18 @@ class Train {
 	get cars() {
 		return this._cars;
 	}
+	buildTrain(trainCar) {
+		if (this._cars.length <= 30) {
+			this._cars.append(trainCar);
+		} else {
+			console.log("The train is either too long, sorry!");
+		}
+	}
+	clipTrain() {
+		if (this._cars.length >= 0) {
+			this._cars.splice(-1, 1);
+		} else {
+			console.log("The train is too short to remove a car");
+		}
+	}
 }
